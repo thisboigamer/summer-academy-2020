70686ebe411adc0eed006489f6bc428694b2dcd5
Mike DeAnda <mdeanda@depaul.edu>
2020-07-03 16:42:11 -0500

    put back unity project with gitignore

---
+++ New Unity Project/Assets/Scripts/enemyCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyCollision : MonoBehaviour
+{
+    //serializeField allows you to assign something to this in Unity
+    
+
+    //this script kills the player when running into a block
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        //first check if the box has run into a player
+        if (collisionInfo.collider.tag == "Player")
+        {
+            FindObjectOfType<GameManager>().EndGame();
+            //if the box runs into a plyer, respawn the player
+            //collisionInfo.collider.transform.position = GameObject.Find("respawnPoint").transform.position;
+            //Physics.SyncTransforms();
+            //destroy the block
+            //Destroy(gameObject);
+        }
+    }
+   
+}
---
+++ New Unity Project/Assets/Scripts/spawnScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class spawnScript : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //check to see if player is the Collider entering the space
+        if(other.CompareTag("Player"))
+        {
+            FindObjectOfType<GameManager>().EndGame();
+
+        }
+    }
+}
